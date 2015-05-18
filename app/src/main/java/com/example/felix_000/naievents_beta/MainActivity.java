aff4c6d08e579c63edf9cfd8998fad6b0c5a62f2
Felix Too <felix.too@caytree.net>
2015-05-18 12:46:06 +0300

    connected to Parse...

--- app/src/main/java/com/example/felix_000/naievents_beta/MainActivity.java
+++ app/src/main/java/com/example/felix_000/naievents_beta/MainActivity.java
@@ -13,6 +13,9 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.parse.Parse;
+import com.parse.ParseObject;
+
 
 public class MainActivity extends ActionBarActivity implements NavigationDrawerFragment.NavigationDrawerCallbacks {
     private NavigationDrawerFragment mNavigationDrawerFragment;
@@ -27,6 +30,13 @@ public class MainActivity extends ActionBarActivity implements NavigationDrawerF
         mNavigationDrawerFragment.setUp(
                 R.id.navigation_drawer,
                 (DrawerLayout) findViewById(R.id.drawer_layout));
+        //Enable Local Datastore
+        Parse.enableLocalDatastore(this);
+        Parse.initialize(this, "yKTdQtjm9QjPKlym7zVFjJrXjbmAX5EncrgUZCbV", "pMSZxLIDpkcgqxIiTYAM1Ybh58uGesRKt5N1nXHf");
+        ParseObject testObject = new ParseObject("TestObject");
+        testObject.put("foo", "bar");
+        testObject.saveInBackground();
+
     }
 
     @Override
